20240462205bd4484259fb2484b6f26bde90b8df
jamesfredio <Jamesfrederick14@icloud.com>
2026-06-24 15:22:51 +0000

    added a minus to the test

--- sum.test.js
+++ sum.test.js
@@ -1,6 +1,11 @@
 import { expect, test } from 'vitest'
-import { sum } from './sum.js'
+import { sum, minus } from './sum.js'
+// this imports the test function
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3)
+})
+
+test('minus 1 - 2 to equal 1', () => {
+  expect(minus(2, 1)).toBe(1)
 })
