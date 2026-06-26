930d45b5e9c1585b5da23ee412e7fdb29ccfa53e
jamesfredio <Jamesfrederick14@icloud.com>
2026-06-26 10:04:11 +0000

    diabled button test

--- my-app/src/lib/components/ui/button/button.test.js
+++ my-app/src/lib/components/ui/button/button.test.js
@@ -1,7 +1,13 @@
 import '@testing-library/jest-dom/vitest';
-import { render, screen } from '@testing-library/svelte';
-import { describe, it, expect } from 'vitest';
+import { render, screen, cleanup } from '@testing-library/svelte';
+import { describe, it, expect, afterEach } from 'vitest';
 import Button from './button.svelte';
+
+afterEach(() => {
+	cleanup();
+});
+//after each test, clear the fake page
+
 
 describe('Button', () => {
 	it('renders a button', () => {
@@ -9,4 +15,16 @@ describe('Button', () => {
 
 		expect(screen.getByRole('button')).toBeInTheDocument();
 	});
+	it('uses button as the default type', () => {
+	render(Button);
+
+	expect(screen.getByRole('button'))
+		.toHaveAttribute('type', 'button');
+});
+	it('should be disabled when disabled prop is true', () => {
+	render(Button, { disabled: true });
+
+	expect(screen.getByRole('button'))
+		.toBeDisabled();
+});
 });
